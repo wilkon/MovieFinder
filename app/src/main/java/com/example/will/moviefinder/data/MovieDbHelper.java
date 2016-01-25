66bd96e019125dba082ecc371cbce89f5ecdd859
Will <tb.wilkon@gmail.com>
2016-01-25 13:19:44 -0500

    setting up content provider -uri -matcher -implemented crud into content provider
    Conflicts:
    	app/app.iml
    	app/src/main/java/com/example/will/moviefinder/data/MovieDbHelper.java

--- app/src/main/java/com/example/will/moviefinder/data/MovieDbHelper.java
+++ app/src/main/java/com/example/will/moviefinder/data/MovieDbHelper.java
@@ -5,20 +5,12 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 /**
-<<<<<<< HEAD
  * Created by will on 1/7/2016.
  */
 public class MovieDbHelper extends SQLiteOpenHelper{
-    private static final int DATABASE_VERSION = 1;
-
-=======
- * Created by will on 1/24/2016.
- */
-public class MovieDbHelper extends SQLiteOpenHelper {
 
     // If you change the database schema, you must increment the database version.
     private static final int DATABASE_VERSION = 2;
->>>>>>> 43dcb4f... setting up content provider
     static final String DATABASE_NAME = "movies.db";
 
     public MovieDbHelper(Context context) {
@@ -27,9 +19,6 @@ public class MovieDbHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-<<<<<<< HEAD
-
-=======
         final String SQL_CREATE_DETAILS_TABLE = "CREATE TABLE " + MoviesContract.DetailsEntry.TABLE_NAME + " (" +
                 MoviesContract.DetailsEntry._ID + " INTEGER PRIMARY KEY," +
                 MoviesContract.DetailsEntry.COLUMN_MOVIE_ID + " TEXT NOT NULL " +
@@ -41,16 +30,12 @@ public class MovieDbHelper extends SQLiteOpenHelper {
                 " );";
 
         db.execSQL(SQL_CREATE_DETAILS_TABLE);
->>>>>>> 43dcb4f... setting up content provider
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-<<<<<<< HEAD
 
-=======
         db.execSQL("DROP TABLE IF EXISTS " + MoviesContract.DetailsEntry.TABLE_NAME);
         onCreate(db);
->>>>>>> 43dcb4f... setting up content provider
     }
 }
